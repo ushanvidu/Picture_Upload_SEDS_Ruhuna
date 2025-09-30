07711fab43bdcd5cefbcca0734d1fe7b8e434031
ushanvidu <136312428+ushanvidu@users.noreply.github.com>
2025-09-30 14:03:25 +0530

    c2

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('dotenv').config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
